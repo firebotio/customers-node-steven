090a6dd60cfd0b78618676ee1d7cba9ff845ed9d
Tommy Dang <quantumventuress@gmail.com>
2015-05-14 21:52:19 -0700

    changed single to double quote

--- app/firebot/service.js
+++ app/firebot/service.js
@@ -2,15 +2,15 @@ var client = require("./client");
 
 var FirebotService = function() {
   endpoint = function(resource) {
-    return client.apiUrl + '/' + client.apiVersion + '/services/' + resource;
+    return client.apiUrl + "/" + client.apiVersion + "/services/" + resource;
   };
 
   this.email = function(data, callback) {
     $.ajax({
       data: data,
       headers: client.headers,
-      method: 'post',
-      url: endpoint('email'),
+      method: "post",
+      url: endpoint("email"),
       success: function(response) {
         if (callback) {
           callback(response);
