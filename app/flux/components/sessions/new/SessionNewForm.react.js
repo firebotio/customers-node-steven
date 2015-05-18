9cd46e6b4eaee3af8a2ae189c216f04442e27ce1
Tommy Dang <quantumventuress@gmail.com>
2015-05-18 10:29:23 -0700

    working on login form

--- app/flux/components/sessions/new/SessionNewForm.react.js
+++ app/flux/components/sessions/new/SessionNewForm.react.js
@@ -1,9 +1,10 @@
-var React = require("react");
-
+var React    = require("react");
 var CryptoJS = require("crypto-js");
+var cookies  = require("../../../../js/cookies");
+var moment   = require("moment");
+var uuid     = require('node-uuid');
 
-var FirebotObject = require("../../../../libs/firebot/FirebotObject");
-
+var FirebotObject    = require("../../../../libs/firebot/FirebotObject");
 var SharedInputField = require("../../shared/SharedInputField.react");
 
 var SessionNewForm = React.createClass({
@@ -62,15 +63,34 @@ var SessionNewForm = React.createClass({
     var _this = this;
 
     if (this.state.email.length > 0 && this.state.password.length > 0) {
-      var organization = null;
       var organizationParams = { email: _this.state.email };
       FirebotObject.index("Organization", organizationParams, function(res) {
-        organization = res.objects[0];
+        var errorMessage = null;
+        var organization = res.objects[0] || null;
         if (organization && organization.password == _this.state.password) {
-          _this.setState({ error: null });
+          var days      = 365;
+          var expiresAt = moment().add(days, "days").format("YYYY-MM-DD");
+          var token     = uuid.v1();
+
+          var hash = {
+            expires_at: expiresAt,
+            organization: organization.id,
+            token: token
+          };
+
+          FirebotObject.create("Authentication", hash, function(response) {
+            cookies.setCookie("sessionId", organization.id, days);
+            cookies.setCookie("sessionToken", response.token, days);
+
+            var sessionIdCookie = cookies.getCookie("sessionId");
+            var sessionTokenCookie = cookies.getCookie("sessionToken");
+
+            console.log(sessionIdCookie, sessionTokenCookie);
+          });
         } else {
-          _this.setState({ error: "Invalid email and/or password" });
+          errorMessage = "Invalid email and/or password";
         }
+        _this.setState({ error: errorMessage });
       });
     } else {
       _this.setState({ error: "Please enter an email and password" });
