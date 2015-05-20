9084dbfc840ae4f951aa39d8f3249fd01f74f169
Tommy Dang <quantumventuress@gmail.com>
2015-05-19 18:08:30 -0700

    updated hero and features on home page

--- app/js/home/index/header.js
+++ app/js/home/index/header.js
@@ -1,19 +1,19 @@
-var homeIndex = $(".home-index");
+// var homeIndex = $(".home-index");
 
-if (homeIndex.length > 0) {
-  $(document).ready(function() {
-    var features    = homeIndex.find(".features");
-    var featuresTop = features.offset().top;
-    var header      = homeIndex.find("header");
+// if (homeIndex.length > 0) {
+//   $(document).ready(function() {
+//     var features    = homeIndex.find(".features");
+//     var featuresTop = features.offset().top;
+//     var header      = homeIndex.find("header");
 
-    $(window).scroll(function() {
-      var windowTop = $(window).scrollTop();
+//     $(window).scroll(function() {
+//       var windowTop = $(window).scrollTop();
 
-      if (windowTop < featuresTop) {
-        header.hide();
-      } else {
-        header.show();
-      }
-    });
-  });
-}
+//       if (windowTop < featuresTop) {
+//         header.hide();
+//       } else {
+//         header.show();
+//       }
+//     });
+//   });
+// }
